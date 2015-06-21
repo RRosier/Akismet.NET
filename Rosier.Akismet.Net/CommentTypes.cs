2c621ea1de80944d6bd0c4f220da56d593bc8e9d
itmeze <michal.zygula@gmail.com>
2015-06-21 23:02:34 +0200

    Enhancing comment types in accordance to http://blog.akismet.com/2012/06/19/pro-tip-tell-us-your-comment_type/

--- Rosier.Akismet.Net/CommentTypes.cs
+++ Rosier.Akismet.Net/CommentTypes.cs
@@ -9,5 +9,11 @@ namespace Rosier.Akismet.Net
         public static readonly string Comment = "comment";
         public static readonly string Trackback = "trackback";
         public static readonly string Pingback = "pingback";
+        public static readonly string ForumPost = "forum-post";
+        public static readonly string BlogPost = "blog-post";
+        public static readonly string ContactForm = "contact-form";
+        public static readonly string Signup = "signup";
+        public static readonly string Tweet = "tweet";
+
     }
 }
